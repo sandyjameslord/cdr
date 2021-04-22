f2e14a9bf40cce46e5c0df762bfffc694c150511
Sandy Lord <sandylord@Sandys-Mac-mini.lan>
2021-04-22 11:54:30 -0400

    adding ownerID to dogs to facilitate adding appointments, show all dogs in sched appt

--- pages/scheduleAppointment.vue
+++ pages/scheduleAppointment.vue
@@ -13,14 +13,14 @@
         
             <div v-for="(dog) in dogs" :key="dog._id">
             <!-- <div v-for="(dog, index) in dogs" :key="dog._id">                 -->
-                <div class='dogDisplay' v-if="(dog.ownerID == this.$route.query.ownerID)">
-                    <div v-if="(this.$route.query.dog == dog.dogName)">
+                <!-- <div class='dogDisplay' v-if="(dog.ownerID == this.$route.query.ownerID)"> -->
+                    <!-- <div v-if="(this.$route.query.dog == dog.dogName)"> -->
                         <div id='dogContainer'>
                             <img class='dogPhoto' :src="dog.photo" alt="">
                             <div class='dogName'>{{dog.dogName}}</div>
                         </div>  
-                    </div>     
-                </div>
+                    <!-- </div>      -->
+                <!-- </div> -->
         </div>
         
                    
